7e256a504faae17e426584697a6f721b02395ff4
Raj Yashasvi Agarwal <100408942+raj-agarwal24@users.noreply.github.com>
2022-04-18 13:40:56 +0530

    Add files via upload

---
+++ introduction/simple calculater.py
@@ -0,0 +1,78 @@
+import math
+print("\t_____CALCULATER_____")
+
+
+def sum(a, b):
+    a -= b
+    return a
+
+
+def sub(a, b):
+    if a > b:
+        a -= b
+        return a
+    else:
+        b -= a
+        return b
+
+
+def mul(a, b):
+    a *= b
+    return a
+
+
+def div(a, b):
+    q = a/b
+    r = a % b
+    print("\nThe quotient is : %s" % q)
+    print("\nThe remainder is : %s" % r)
+
+
+def sqr(a):
+    x = math.sqrt(a)
+    return x
+
+
+while (True):
+    print("\nChoose the operation you want to perform: ")
+    print("\n\t1.ADDITION")
+    print("\n\t2.SUBTRACTION")
+    print("\n\t3.MULTIPLICATION")
+    print("\n\t4.DIVISION")
+    print("\n\t5.SQUAREROOT")
+    print("\n\t6.EXIT")
+
+    choice = int(input(">"))
+    if choice == 1:
+        print("\n\nEnter the two numbers: ")
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        s = sum(num1, num2)
+        print("The sum is : %s" % s)
+    elif choice == 2:
+        print("\n\nEnter the two numbers: ")
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        m = sub(num1, num2)
+        print("The diffrence is : %s" % m)
+    elif choice == 3:
+        print("\n\nEnter the two numbers: ")
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        p = mul(num1, num2)
+        print("The product is : %s" % p)
+    elif choice == 4:
+        print("\n\nEnter the two numbers: ")
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        div(num1, num2)
+        print("The sum is : %s" % s)
+    elif choice == 5:
+        print("\n\nEnter the two numbers: ")
+        num1 = int(input(">"))
+        num2 = int(input(">"))
+        r = sqr(num1,)
+        print("The square root is : %s" % r)
+    else:
+        print("\nYOU CHOOSE TO EXIT, BYE.....")
+        break
